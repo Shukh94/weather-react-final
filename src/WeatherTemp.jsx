a3ccf9d771d4acfcb4772ea629b68c619eade60e
Shukh94 <shakila.sk09@gmail.com>
2024-10-24 21:44:43 +0600

    deleted fahrenheit convertion

--- src/WeatherTemp.jsx
+++ src/WeatherTemp.jsx
@@ -1,51 +1,17 @@
-import { useState } from "react";
+
 import "./WeatherTemp.css";
 
 function WeatherTemp(prop) {
-  const [unit, setUnit] = useState("celsius");
-
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  function fahrenheit() {
-    return (prop.celsius * 9) / 5 + 32;
-  }
-
-  if (unit === "celsius") {
-    return (
-      <div className="WeatherTemp">
-        <span className="temperature">
-          {Math.round(prop.celsius)}
-          <span className="unit">
-            °C|
-            <a href="/" onClick={showFahrenheit}>
-              °F
-            </a>
-          </span>
+  return (
+    <div className="WeatherTemp">
+      <span className="temperature">
+        {Math.round(prop.celsius)}
+        <span className="unit">
+          °C
         </span>
-      </div>
-    );
-  } else {
-    return (
-      <div className="WeatherTemp">
-        <span className="temperature">
-          {Math.round(fahrenheit())}
-          <span className="unit">
-            <a href="/" onClick={showCelsius}>
-              °C
-            </a>
-            |°F
-          </span>
-        </span>
-      </div>
-    );
-  }
+      </span>
+    </div>
+  );
 }
 
 export default WeatherTemp;
